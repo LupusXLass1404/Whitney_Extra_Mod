2fd85a8bad3d5ef127ae58ae792f811c8a7a54a5
LupusXLass1404 <BerkasXLupusXLass@gmail.com>
2024-02-19 20:07:09 +0800

    v1.0.0

--- Whitney_Extra_Mod/README.md
+++ Whitney_Extra_Mod/README.md
@@ -1,5 +1,6 @@
-## 惠特尼附加模組 Whitney Extra Mod
+## Whitney Extra Mod
 - 此Mod需要用ModLoader加載。
+- 只有中文版本，沒有英文版本。
 - 二創皆有OOC，小心食用。
 - 模組非遊戲本體，如果要分享模組相關的截圖／內容，請務必補充說明是模組內容而非原版內容，避免造成不必要的誤會。
 - [模組文本整理（Google文件）](https://docs.google.com/document/d/1yQoYOq_Tn64dyLmVLmA3p115v8DQgRyX__M0dL0847I/edit?usp=sharing)：給不想下載模組，但是想看文本的人。
@@ -7,6 +8,5 @@
 
 ## 更新內容
 - v1.0.0
-  - 增新惠特尼湖邊餵鴨子事件
-  - 他會在沒下雨的夜晚出現在湖邊
-  - 可以提供爆米花
+  - 增新惠特尼湖泊餵鴨子事件。他會在陰天的夜晚出現在湖泊岸邊，冬天不會出現。
+  
--- Whitney_Extra_Mod/game/03-JavaScript/whitney_extra_mod_fn.js
+++ Whitney_Extra_Mod/game/03-JavaScript/whitney_extra_mod_fn.js
@@ -1,10 +1,11 @@
 function whitExtMod_isInLakeDuck() {
     return C.npc.Whitney.init === 1
         && C.npc.Whitney.state == "active"
-        && !["snow", "rain"].includes(V.weather) //不是下雪或雨天
+        && ["overcast"].includes(V.weather) //陰天
         && Time.hour >= 21 //晚上9點後
         && Time.weekDay !== 1 //不是星期天
         && Time.season !== "winter" //不是冬天
+        && V.halloween !== 1 //不是萬聖節
         && V.pillory_tenant.special.name !== "Whitney" //惠特尼不在頸手枷 
         && V.daily.whitney.whitExtMod_lake !== 0;
 } window.whitExtMod_isInLakeDuck = whitExtMod_isInLakeDuck;
